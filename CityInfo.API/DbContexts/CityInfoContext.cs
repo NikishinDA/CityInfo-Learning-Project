2d1aaf416b284aded423ab1bd7285b23d6e1910c
NikishinDA <dan8427@gmail.com>
2023-07-12 21:13:13 +0400

    db migrations

--- CityInfo.API/DbContexts/CityInfoContext.cs
+++ CityInfo.API/DbContexts/CityInfoContext.cs
@@ -8,6 +8,11 @@ namespace CityInfo.API.DbContexts
         public DbSet<City> Cities { get; set; } = null!;
         public DbSet<PointOfInterest> PointsOfInterest { get; set; } = null!;
 
+        public CityInfoContext(DbContextOptions<CityInfoContext> options) : base(options)
+        {
+            
+        }
+
         //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         //{
         //    optionsBuilder.UseSqlite("connectionstring");
