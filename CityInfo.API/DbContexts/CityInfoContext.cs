e8561ab145039e8a61c27c217189292b6f12d4d0
NikishinDA <dan8427@gmail.com>
2023-07-12 21:53:39 +0400

    seeding db

--- CityInfo.API/DbContexts/CityInfoContext.cs
+++ CityInfo.API/DbContexts/CityInfoContext.cs
@@ -1,4 +1,5 @@
 ﻿using CityInfo.API.Entities;
+using CityInfo.API.Models;
 using Microsoft.EntityFrameworkCore;
 
 namespace CityInfo.API.DbContexts
@@ -10,7 +11,68 @@ namespace CityInfo.API.DbContexts
 
         public CityInfoContext(DbContextOptions<CityInfoContext> options) : base(options)
         {
-            
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<City>().HasData(
+                 new City("New York City")
+                 {
+                     Id = 1,
+                     Description = "The one with big park.",
+
+                 },
+                new City("Antwerp")
+                {
+                    Id = 2,
+                    Description = "The one with the cathedral that was newer really finished.",
+
+                },
+                new City("Paris")
+                {
+                    Id = 3,
+                    Description = "The one with that big tower.",
+                });
+            modelBuilder.Entity<PointOfInterest>().HasData(
+                new PointOfInterest("Central Park")
+                {
+                    Id = 1,
+                    CityId = 1,
+                    Description = "The most visited urban park in the US."
+                },
+                new PointOfInterest("Empire State Building")
+                {
+                    Id = 2,
+                    CityId = 1,
+                    Description = "A 102-story skyscraper located in Midtown Manhattan."
+                },
+                new PointOfInterest("Cathedral of Our Lady") 
+                { 
+                    Id = 3, 
+                    CityId = 2, 
+                    Description = "A gothic style cathedral." 
+                },
+                new PointOfInterest("Antwerp Central Station")
+                {
+                    Id = 4,
+                    CityId = 2,
+                    Description = "Belgum's architecture."
+
+                },
+                new PointOfInterest("Eiffel Tower")
+                {
+                    Id = 5,
+                    CityId =3,
+                    Description = "Big iron tower."
+                },
+                new PointOfInterest("The Louvre")
+                {
+                    Id = 6,
+                    CityId = 3,
+                    Description = "Big museum."
+                });
+            base.OnModelCreating(modelBuilder);
         }
 
         //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
